859188e0a46b3a92c181cb87ed99041b41c449ae
Elvann <elvann.abendroth@gmail.com>
2023-04-13 15:55:02 +0300

    🗑️Started from scratch with Rect

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <p>
+          GitHub Codespaces <span className="heart">♥️</span> React + Github Pages
+        </p>
+
+        <p className="small">Use this as a template to to start your react project & host it with Github Pages!</p>
+      
+        <p>
+          <a
+            className="App-link"
+            href="https://github.com/ElvannAbendroth/react-page-template"
+            target="_blank"
+            
+          >
+            Checkout Repository
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
